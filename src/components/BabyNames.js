026a5aef608b538cc58ca805b2f671cc8f67f91f
koakande <koakande@gmail.com>
2021-04-05 21:24:49 +0100

    add sorting function to arrange the baby names in ascending order

--- src/components/BabyNames.js
+++ src/components/BabyNames.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import babyNames from '../babyNames.json';
 
+
+//  sort the array object list 
+babyNames.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+
+
 // return 
 const ListName = () => {
     return (
