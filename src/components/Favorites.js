4557fc7d1d0cfcd8a170f8591c554108e32f7af3
koakande <koakande@gmail.com>
2021-04-06 14:34:24 +0100

    add Favorites component for listing favourites names

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,8 +1,12 @@
-const Favorites = () => {
-
+const Favorites = (props) => {
+    const favNames = props.favNames;
     return (
         <div className=".favourites-container">
             <p className="favourites-list"> Favourites: </p>
+            { favNames.map((favName, index) => {
+                return <p key={index}> {favName.name} </p>
+            })
+            }
         </div>
     )
 
