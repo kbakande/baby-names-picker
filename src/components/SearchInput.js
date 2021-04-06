ab21cdc516daa8d3140536d1a6163af95ef98282
koakande <koakande@gmail.com>
2021-04-06 20:30:09 +0100

    remove changes done in orderr to put the logic for names search here

---
+++ src/components/SearchInput.js
@@ -0,0 +1,17 @@
+// write logic for getting search names
+const searchInput = (event, props) => {
+    console.log(props)
+    // const inputSearch = event.target.value.toLowerCase();
+
+    // if (inputSearch) {
+    //     const searchNames = names.filter((babyName) => {
+    //         return babyName.name.toLowerCase().includes(inputSearch);
+    //     });
+    //     setNames(searchNames);
+    // } else {
+    //     setNames(names);
+    // }
+};
+
+
+export default searchInput
