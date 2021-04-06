d53e19e4e0494b34430076e12e98eb0543418477
koakande <koakande@gmail.com>
2021-04-06 08:13:13 +0100

    refactor code to extract elements into individual components

--- src/components/BabyNames.js
+++ src/components/BabyNames.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import NamesList from './NamesList'
+import SearchBar from './SearchBar'
+import HorizontalLine from './HorizontalLine'
 import babyNames from '../babyNames.json';
 
 
@@ -6,36 +9,17 @@ import babyNames from '../babyNames.json';
 babyNames.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
 
 
-// return 
+// return the container for the names
 const ListName = () => {
 
     return (
         <div className="name-container">
-            {/* <div className="search-bar"> <SearchBar /> </div> */}
             <SearchBar />
-            <hr></hr>
-            {babyNames.map((babyName, index) => {
-                if (babyName.sex === "m") {
-                    return (
-                        <p className="name-list boys" key={index}> {babyName.name} </p>
-                    )
-                } else {
-                    return (
-                        <p className="name-list girls" key={index}> {babyName.name} </p>
-                    )
-                }
-            })}
-            <hr></hr>
+            <HorizontalLine />
+            <NamesList babyNames={babyNames} />
+            <HorizontalLine />
         </div >
     )
-}
-
-const SearchBar = () => {
-
-    return (
-        <input type="input" id="searchInput" placeholder="Search for a name..." className="search-bar"></input>
-    )
-
 }
 
 export { ListName }
