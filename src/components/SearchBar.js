9d49fb772fed590d6eca3571b81b79738a16a74f
koakande <koakande@gmail.com>
2021-04-06 08:17:33 +0100

    adds a component to return a search input element

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+const SearchBar = () => {
+
+    return (
+        <input type="input" id="searchInput" placeholder="Search for a name..." className="search-bar"></input>
+    )
+
+}
+
+
+export default SearchBar
