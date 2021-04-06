5731756baca953b62eb1512c5d43a78b297c794e
koakande <koakande@gmail.com>
2021-04-06 22:52:25 +0100

    adds logic to order names before displaying

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,9 +1,9 @@
 import NamesList from "./NamesList";
 
 const Favorites = (props) => {
-    const favNames = props.favNames;
+    const favNames = props.favNames.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));;
     return (
-        <div className=".favourites-container">
+        <div className=".favourites-container" onClick={props.handleNamesReturn}>
             <p className="favourites-list"> Favourites: </p>
             <NamesList babyNames={favNames} />
         </div>
