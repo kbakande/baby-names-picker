aca662e1aadfb2acaab8efd63c7ee61593f348ec
koakande <koakande@gmail.com>
2021-04-06 12:45:49 +0100

    add a component to return favorites names

---
+++ src/components/Favorites.js
@@ -0,0 +1,11 @@
+const Favorites = () => {
+
+    return (
+        <div className=".favourites-container">
+            <p className="favourites-list"> Favourites: </p>
+        </div>
+    )
+
+}
+
+export default Favorites
