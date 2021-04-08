0c945e842f3a170b4fb9f8f734e7f6ddf3cb5935
koakande <koakande@gmail.com>
2021-04-08 15:32:39 +0100

    remove div around names list components

--- src/components/NamesList.js
+++ src/components/NamesList.js
@@ -5,24 +5,26 @@ const NamesList = props => {
     const babyNames = props.babyNames.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));;
     return (
 
-        <div>
-            {babyNames.map((babyName, index) => {
-                if (babyName.sex === "m") {
-                    return (
-                        <p className="name-list boys" key={index} onClick={props.favFunc}> {babyName.name}  </p>
+        // <div className="names-list">
+        // {
+        babyNames.map((babyName, index) => {
+            if (babyName.sex === "m") {
+                return (
+                    <p className="name-list boys" key={index} onClick={props.favFunc}> {babyName.name}  </p>
 
-                        // <div> <FaHeart /> {babyName.name}</div>
-                    )
-                } else {
-                    return (
-                        <p className="name-list girls" key={index} onClick={props.favFunc}> {babyName.name}</p>
+                    // <div> <FaHeart /> {babyName.name}</div>
+                )
+            } else {
+                return (
+                    <p className="name-list girls" key={index} onClick={props.favFunc}> {babyName.name}</p>
 
-                        // <p className="name-list girls" key={index} onClick={props.favFunc}> {babyName.name}</p>
-                    )
-                }
-            })}
+                    // <p className="name-list girls" key={index} onClick={props.favFunc}> {babyName.name}</p>
+                )
+            }
+        })
+        // }
 
-        </div>
+        // </div>
 
     )
 }
