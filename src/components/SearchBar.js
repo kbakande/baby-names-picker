419f04f56ed7b60fb0730f09b47793f075dc8ba8
koakande <koakande@gmail.com>
2021-04-08 15:33:17 +0100

    adds icons for retirveing sec tions of nasmes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,16 @@
 
-import { GiPlasticDuck } from "react-icons/gi";
-
+import { IoManOutline, IoWomanOutline, IoShuffle } from "react-icons/io5";
+import { GrAnchor } from "react-icons/gr";
 const SearchBar = (props) => {
 
     return (
         <div className="search-container">
+            <IoShuffle className="duck-icon all" />
             <input onChange={props.search} type="input" id="searchInput" placeholder="Search for a name..." className="search-bar"></input>
-            {/* <p className='icon-list active'> <GiPlasticDuck className="duck-icon" /> </p>
-            <p className='icon-list active'> <GiPlasticDuck className="duck-icon" /> </p> */}
-            <GiPlasticDuck className="duck-icon active" />
-            <GiPlasticDuck className="duck-icon active" />
-            <GiPlasticDuck className="duck-icon active" />
+            {/* <GiPlasticDuck className="duck-icon" /> */}
+            <GrAnchor className="duck-icon all" />
+            <IoWomanOutline className="duck-icon girls" />
+            <IoManOutline className="duck-icon boys" />
         </div>
     )
 
