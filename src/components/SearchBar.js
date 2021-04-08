9c9fc1572aa17fc62f61de2f10e708981eac6e98
koakande <koakande@gmail.com>
2021-04-08 22:12:11 +0100

    adds onclick to handle girl/boy names retrieval through icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,12 @@ const SearchBar = (props) => {
 
     return (
         <div className="search-container">
-            <IoShuffle className="duck-icon all" />
+            <IoShuffle className="duck-icon shuffle" />
             <input onChange={props.search} type="input" id="searchInput" placeholder="Search for a name..." className="search-bar"></input>
             {/* <GiPlasticDuck className="duck-icon" /> */}
             <GrAnchor className="duck-icon all" />
             <IoWomanOutline className="duck-icon girls" onClick={props.girlHandler} />
-            <IoManOutline className="duck-icon boys" />
+            <IoManOutline className="duck-icon boys" onClick={props.boyHandler} />
         </div>
     )
 }
