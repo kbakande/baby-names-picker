7904ff5828aefe3822c8ec8712d544da59713825
koakande <koakande@gmail.com>
2021-04-08 13:19:21 +0100

    adds font awesome icons for selecting baby genders

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,17 @@
+
+import { GiPlasticDuck } from "react-icons/gi";
+
 const SearchBar = (props) => {
 
     return (
-        <input onChange={props.search} type="input" id="searchInput" placeholder="Search for a name..." className="search-bar"></input>
+        <div className="search-container">
+            <input onChange={props.search} type="input" id="searchInput" placeholder="Search for a name..." className="search-bar"></input>
+            {/* <p className='icon-list active'> <GiPlasticDuck className="duck-icon" /> </p>
+            <p className='icon-list active'> <GiPlasticDuck className="duck-icon" /> </p> */}
+            <GiPlasticDuck className="duck-icon active" />
+            <GiPlasticDuck className="duck-icon active" />
+            <GiPlasticDuck className="duck-icon active" />
+        </div>
     )
 
 }
