2647077edd05b2900ec9d7b55bb07beac9fd79fd
koakande <koakande@gmail.com>
2021-04-08 15:30:53 +0100

    adds a container

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,7 +3,7 @@ import NamesList from "./NamesList";
 const Favorites = (props) => {
     const favNames = props.favNames.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));;
     return (
-        <div className=".favourites-container" onClick={props.handleNamesReturn}>
+        <div className="favourites-container" onClick={props.handleNamesReturn}>
             <p className="favourites-list"> Favourites: </p>
             <NamesList babyNames={favNames} />
         </div>
