289158280975ceb7c61f03f5500806fc6a4f0fb2
koakande <koakande@gmail.com>
2021-04-09 11:48:24 +0100

    adds logic for shuffling

---
+++ src/components/ShuffleNames.js
@@ -0,0 +1,20 @@
+import NamesList from "./NamesList";
+
+const Shuffle = (props) => {
+    const shuffledNames = shuffleArray(props.favNames);
+    return (
+        <div className="favourites-container" onClick={props.handleNamesReturn}>
+            <p className="favourites-list"> Favourites: </p>
+            <NamesList babyNames={shuffledNames} />
+        </div>
+    )
+
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+export default Favorites
