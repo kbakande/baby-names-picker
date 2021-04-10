0cb790c0e8e8120cfce2ba00e5aa89308799882f
koakande <koakande@gmail.com>
2021-04-10 07:21:51 +0100

    added instructions to explain favourites section

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,9 +3,11 @@ import NamesList from "./NamesList";
 const Favorites = (props) => {
     const favNames = props.favNames.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));;
     return (
-        <div className="favourites-container" onClick={props.handleNamesReturn}>
-            <p className="favourites-list"> Favourites: </p>
-            <NamesList babyNames={favNames} />
+        <div>
+            <p className="favourites-list" > Favourites: Click some names below to add to your shortlist...</p>,
+            <div className="favourites-container" onClick={props.handleNamesReturn}>
+                <NamesList babyNames={favNames} />
+            </div>
         </div>
     )
 
