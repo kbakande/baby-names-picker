f663a85b4c7f82008a9f90e2243c0d5ddfef9327
koakande <koakande@gmail.com>
2021-04-10 07:22:17 +0100

    create Header components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { GrSelect } from "react-icons/gr";
+
+const Header = () => {
+    return (
+        <div header-container>
+            <GrSelect className="header-icon" />
+            <p className="header-text"> Baby Name Picker</p>
+        </div>
+    )
+}
+
+export default Header
